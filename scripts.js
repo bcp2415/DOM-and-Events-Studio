50520dce90520679ba41d52a88b38969c027ebd6
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-12 12:15:24 -0600

    adds respondToTakeoff fcn

--- scripts.js
+++ scripts.js
@@ -2,14 +2,28 @@
 // Remember to pay attention to page loading!
 
 window.addEventListener("load", init);
+let height = 0;
 
 function init() {
-   declareConstants();
-
+    declareConstants();
+    takeoff.addEventListener("click", respondToTakeoff);
 }
 
 function declareConstants() {
     const takeOff = document.getElementById("takeoff");
     const landing = document.getElementById("landing");
     const missionAbort = document.getElementById("missionAbort");
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+}
+
+function respondToTakeoff() {
+    const response = window.confirm("Confirm that the shuttle is ready for takeoff.")
+    if (response) {
+        flightStatus.innerText = "Shuttle in flight.";
+        shuttleBackground.style.backgroundColor = "blue";
+        height += 10000;
+        spaceShuttleHeight.innerText = height;
+    }
 }
